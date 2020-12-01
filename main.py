01ffbd133560420bb579d204f4e3ce2ec184095d
Aditya Bhosle <39487795+adibhosle@users.noreply.github.com>
2020-12-01 14:56:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -37,9 +37,12 @@ cnn.add(keras.layers.Dense(units=1, activation='sigmoid'))
 cnn.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 cnn.fit(x=training_set, validation_data=test_set, epochs=25)
 
+# MODEL PREDICTION
 
 import numpy as np
 from keras.preprocessing import image
+# Plz ensure that cat_or_dog_1.jpg is renamed for the existing img in the dataset for predicting
+# plz use own renamed file for prediction
 test_image = image.load_img('cat_or_dog_1.jpg', target_size = (64, 64))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
